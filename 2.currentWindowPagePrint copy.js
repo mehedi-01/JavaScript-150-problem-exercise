53875e080b300f18bd27bc94f30bef86d0e0aea3
Md Mehedi Hasan <45948603+mehedi-01@users.noreply.github.com>
2021-11-16 22:36:57 +0600

    new

---
+++ 2.currentWindowPagePrint copy.js
@@ -0,0 +1,7 @@
+
+function CurrentPage() {
+
+    window.print();
+
+
+}
---
+++ 3.current_date.js
@@ -0,0 +1,22 @@
+var today = new Date ();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+
+if (dd <10){
+
+    dd = '0' +dd;
+
+}
+if (mm <10) {
+
+    mm = '0' +mm;
+
+}
+today = mm+'.'+dd+'.'+yyyy;
+console.log(today);
+today = mm+'-'+dd+'-'+yyyy;
+console.log(today);
+today = mm+'/'+dd+'/'+yyyy;
+console.log(today);
